281122d412e22325b00333d34c2cc0f2ba0ab607
Anastasia <n41111215@mail.ru>
2020-06-22 23:37:27 +0300

    Add @EqualsAndHashCode(callSuper = true)

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -1,13 +1,19 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
 
 public class Book extends Product {
     private String author;
-
-    public Book() {
-        super();
-    }
 
     public Book(int id, String name, int price, String author) {
         super(id, name, price);
@@ -22,25 +28,6 @@ public class Book extends Product {
         this.author = author;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Book)) return false;
-        if (!super.equals(o)) return false;
-        Book book = (Book) o;
-        return getAuthor().equals(book.getAuthor());
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getAuthor());
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "author='" + author + '\'' +
-                '}';
-    }
 }
 
--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -1,13 +1,20 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
 
 public class Smartphone extends Product {
     private String maker;
 
-    public Smartphone() {
-        super();
-    }
 
     public Smartphone(int id, String name, int price, String maker) {
         super(id, name, price);
@@ -20,26 +27,5 @@ public class Smartphone extends Product {
 
     public void setMaker(String maker) {
         this.maker = maker;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Smartphone)) return false;
-        if (!super.equals(o)) return false;
-        Smartphone that = (Smartphone) o;
-        return getMaker().equals(that.getMaker());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getMaker());
-    }
-
-    @Override
-    public String toString() {
-        return "Smartphone{" +
-                "maker='" + maker + '\'' +
-                '}';
     }
 }
